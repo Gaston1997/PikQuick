83f335561b03bc2d1a2f0e70c28c6ac283943ae0
Gaston1997 <fortuna_gaston@hotmail.com>
2015-08-28 08:39:35 -0300

    Creo home.html para empezar a trabajar con los posts . Agrego en las views home.html para renderizarla

--- PikQuick/blog/views.py
+++ PikQuick/blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response, render, redirect
+#from blog.models import Entrada
+from django.core.mail import EmailMessage
 
 # Create your views here.
+def home(request):
+    context = RequestContext(request)
+    #posts = Entrada.objects.filter(published = True)
+    return render_to_response('home.html',
+                              {},
+                              context)
