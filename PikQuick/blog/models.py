26b7f1b709e71adeff98c16792663b495cc3affb
Gaston1997 <fortuna_gaston@hotmail.com>
2015-09-04 10:32:26 -0300

    Saco el autor del model , hasta que tengamos LogIn

--- PikQuick/blog/models.py
+++ PikQuick/blog/models.py
@@ -11,12 +11,12 @@ class Entrada(models.Model):
 
     titulo = models.CharField(u'Título', max_length = 100)
     fecha = models.DateTimeField(u'Fecha del Post',auto_now_add=True)
-    #resumen= models.CharField(u'Resumen',max_length=512)
-    #contenido = models.TextField(u'Contenido')
     img = models.FileField(u'Imagen de portada',upload_to = 'documents', default='null')
     published = models.BooleanField(u'Publicado?', default=True)
-    autor = models.ForeignKey(User)
+    #autor = models.ForeignKey(User)
     #categoria = models.ManyToManyField('Categoria')
+    #resumen= models.CharField(u'Resumen',max_length=512)
+    #contenido = models.TextField(u'Contenido')
 
     def __str__(self):
         return self.titulo
